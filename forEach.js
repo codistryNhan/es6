a1c64ae822df06d242880e285eb691db6e63a0a6
nhan <nhan@codistry.io>
2018-03-04 16:59:48 -0800

    Added forEach

---
+++ forEach.js
@@ -0,0 +1,37 @@
+/*
+*  forEach Array Helper
+*/
+
+
+/*
+var colors = [ 'red', 'blue', 'green'];
+
+for(var i = 0; i < colors.length; i++){
+  console.log(colors[i]);
+}
+
+colors.forEach(function(color){
+  console.log(color);
+})
+*/
+
+
+
+var numbers = [1,2,3,4,5];
+
+var sum = 0;
+
+/*
+numbers.forEach(function(number){
+  sum += number;
+});
+*/
+
+function adder(num){
+  sum += num;
+}
+
+console.log(sum);
+
+
+
