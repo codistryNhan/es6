21d303212f30047cc23331af3298d63a24807d31
nhan <nhan@codistry.io>
2018-03-04 21:08:59 -0800

    Added map array helper

---
+++ map.js
@@ -0,0 +1,33 @@
+/*
+*  Array Map Helper
+*/
+
+/*
+var numbers = [1,2,3];
+var doubledNumbers = [];
+*/
+
+/*
+for(var i = 0; i < numbers.length; i++){
+  doubledNumbers.push(numbers[i] * 2);
+}
+*/
+
+/*
+var doubled = numbers.map(function(number){
+  return number * 2;
+});
+
+console.log(doubled);
+*/
+
+var cars = [
+  {model: 'Buick', price: 'Cheap'},
+  {model: 'Camaro', price: 'expensive'}
+];
+
+var prices = cars.map(function(car){
+  return car.price;
+})
+
+console.log(prices);
